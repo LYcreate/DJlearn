aa016eedaa1f0162e0df6f4aa724f00e9caadf14
LYyouth <1141786261@qq.com>
2018-01-22 10:40:27 +0800

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'dj_db1',
-        'USER': 'root',
-        'PASSWORD': 'lymariadb',
+        'USER': '#',
+        'PASSWORD': '#',
         'HOST': '',
         'PORT': '',
     }
